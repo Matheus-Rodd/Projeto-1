5854576cbcb5fc28b172be94a350ba0db63de185
Matheus-Rodd <mathrodd132@gmail.com>
2025-07-31 21:45:27 -0300

    Att. 31/07

---
+++ recipes/models.py
@@ -0,0 +1,20 @@
+# Create your models here.
+
+# recipes/models.py
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    cooking_time = models.IntegerField(help_text="Em minutos")
+
+    image = models.ImageField(uploud_to='recipes/',blank=True, null=True)
+
+
+def __str__(self):
+    return self.title
+
+class Meta:
+
+    verbose_name_plural = 'Recipes'
---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+def recipe_detail(request, recipe_pk):
+    recipe = get_object_or_404(Recipe, pk=recipe_pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
