0a8caf3d809ad709ec10dbde91c3ad7ade53564b
Matheus-Rodd <mathrodd132@gmail.com>
2025-08-06 21:07:00 -0300

    Att. Projeto PT.2

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
-# recipes/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
-    path('<int:pk/', views.recipe_detail, name='recipe_detail'),
-    ]
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
-
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
-def recipe_detail(request, recipe_pk):
-    recipe = get_object_or_404(Recipe, pk=recipe_pk)
+
+def recipe_detail(request, pk):  # <-- Corrigido aqui
+    recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
